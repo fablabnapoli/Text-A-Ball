dae2e986f864e7284e7daaafe11583901dff951e
SaXeee <dibenedetto.salvatore@gmail.com>
2016-10-07 13:05:03 +0200

    Distinta la webUI dal tweet grabber; Altre modifiche minori

--- Dispatcher/bin/tweet2DB.py
+++ Dispatcher/bin/tweet2DB.py
@@ -1,11 +1,14 @@
 import __builtin__
 import sqlite3
-import web
 import tweepy
-import sys
 from tweetStreamer import twitterStreamClient
+import sys
 import ConfigParser
 from tweetLib import *
+
+
+################################################################################
+#				Leggo il contenuto del file di configurazione
 
 # Leggo i parametri di configurazione del dispatcher
 Config = ConfigParser.ConfigParser()
@@ -25,66 +28,31 @@ for key, val in Config.items("textToTrack"):
 dbName = Config.get("dbName", "dbName")
 
 
-# Aggiungo la variabile dbConn a quelle super-globali e la inizializzo a None
-# __builtin__.dbConn = None
+################################################################################
+#						Avvio la connessione al DB
 
 # Mi connetto al DB ed aggiungo la connessione alla var Super-Globali
 # Attenzione: la connessione al DB deve essere definita per essere visibile
 # a tutte le classi/istanze.
 __builtin__.dbConn = sqlite3.connect(dbName)
 
-# Elenco delle pagine gestire dalla web application e relative
-# classi associate.
-urls = (
-   '/', 'Index',
-   '/shutdown', 'Shutdown', 
-)
 
-class Index:
-	
-	dbConn = None
-	
-	def __init__(self):
-		
-		# Web.py crea un thread per ogni richiesta http.
-		# Gli oggetti __builtin__ sono relativi solo al thred in cui sono stati creati
-		# Per cui devo creare una connessione per ogni request HTTP
-		__builtin__.dbConn = sqlite3.connect(dbName)
-	
-	def GET(self):
-		
-		# Estraggo l'elenco dei Tweet
-		tweetList = TweetList()
-		
-		# Se i dati post contengono un'indicazione di filtro
-		# tweetList.setFilters({'user':"pluto"})
-		
-		return render.index(tweetList = tweetList.getTweets())
-	
-	def __del__(self):
-		__builtin__.dbConn.close()
-
-# Attivazione della web application
-app = web.application(urls, globals())
-# Attivazione del motore dei template
-render = web.template.render('templates/')
+################################################################################
+#						Ciclo principale
 
 if __name__ == "__main__":
 	try:
 		
-		# Avvio della ControlInterface web 
-		app.run()
-		
 		# Istanza tweetStreamerClient
 		# tsc = twitterStreamClient(authData, txtToTrack)
-		# twitterStreamClient(authData, txtToTrack)
+		twitterStreamClient(authData, txtToTrack)
 
 		# Avvio l'hubListner
 		# 
 		
 	except sqlite3.Error as e:
 		print "Sqlite error:\n %s" % e.args[0]
-	except tweepy.TweepError as e:
+	except tweepy.TweetError as e:
 		print ("Tweepy error:\n %s" %(e))
 	except Exception, e:
 		print ("General error:\n %s" %(e))
---
+++ Dispatcher/bin/webUI.py
@@ -0,0 +1,76 @@
+import __builtin__
+import sqlite3
+import web
+import sys
+import ConfigParser
+from tweetLib import *
+
+###############################################################################
+#				Leggo il contenuto del file di configurazione
+
+# Leggo i parametri di configurazione del dispatcher
+Config = ConfigParser.ConfigParser()
+Config.read("config.ini")
+
+# Recupero il nome del DataBase
+dbName = Config.get("dbName", "dbName")
+
+################################################################################
+#				Configurazione della web interface
+
+# Elenco delle pagine gestire dalla web application e relative
+# classi associate.
+urls = (
+   '/', 'Index',
+   '/shutdown', 'Shutdown', 
+)
+
+# Classe per la gestione della pagina index (/)
+class Index:
+	
+	dbConn = None
+	
+	def __init__(self):
+		
+		# Web.py crea un thread per ogni richiesta http.
+		# Gli oggetti __builtin__ sono relativi solo al thred in cui sono stati creati
+		# Per cui devo creare una connessione per ogni request HTTP
+		__builtin__.dbConn = sqlite3.connect(dbName)
+	
+	def GET(self):
+		
+		# Estraggo l'elenco dei Tweet
+		tweetList = TweetList()
+		
+		# Se i dati post contengono un'indicazione di filtro
+		# tweetList.setFilters({'user':"pluto"})
+		
+		return render.index(tweetList = tweetList.getTweets())
+	
+	def __del__(self):
+		__builtin__.dbConn.close()
+
+# Attivazione della web application
+app = web.application(urls, globals())
+
+# Attivazione del motore dei template.
+# I files .html dovranno essere posizionati nella catella teamplates/
+# I file statici (immagini, css, javascript) nella cartella static/
+render = web.template.render('templates/')
+
+
+################################################################################
+#						Ciclo principale
+
+if __name__ == "__main__":
+	try:
+		
+		# Avvio della ControlInterface web 
+		app.run()
+
+		
+	except sqlite3.Error as e:
+		print "Sqlite error:\n %s" % e.args[0]
+
+	except Exception, e:
+		print ("General error:\n %s" %(e))
